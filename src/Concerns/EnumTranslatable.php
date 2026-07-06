b597f522bfd187a711e81d3cfc6f9eeafa165faf
Hammed Oyedele <itz.harmid@gmail.com>
2026-07-06 17:06:04 +0100

    fix: refine getTransKey method for accurate translation key generation

--- src/Concerns/EnumTranslatable.php
+++ src/Concerns/EnumTranslatable.php
@@ -28,7 +28,7 @@ trait EnumTranslatable
     public static function getTransKey(): string
     {
         $namespace = static::getTranslationNamespace();
-        $key = str(rtrim(class_basename(static::class), 'Enum'))->snake()->plural();
+        $key = str(static::class)->classBasename()->beforeLast('Enum')->snake()->plural();
 
         return $namespace ? "$namespace::enums.$key" : "enums.$key";
     }
